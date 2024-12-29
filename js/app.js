11acfbdc917e74c72a0bdf6b7cb608f705a3ff59
alextorresp <124532842+alextorresp@users.noreply.github.com>
2024-12-28 16:17:01 -0800

    Create function to insert commas into display value

--- js/app.js
+++ js/app.js
@@ -1,30 +1,55 @@
 let currentValue = 0;
 
-const displayedNumber = document.getElementById('displayed-number');
+const displayedValue = document.getElementById('displayed-value');
 const buttonsContainer = document.getElementById('calc-container');
 
 buttonsContainer.addEventListener('click', (event) => {
   const button = event.target;
 
   if (button.hasAttribute('data-number')) {
-    displayNumber(button.getAttribute('data-number'));
+    updateValue(button.getAttribute('data-number'));
   } else if (button.hasAttribute('data-operation')) {
     handleOperation(button.getAttribute('data-operation'));
   };
 });
 
-function displayNumber(number) {
-  if (currentValue === 0) {
+function updateValue(number) {
+  const currentValueLength = currentValue.toString().length;
+  if (currentValueLength === 9) {
+    return;
+  } else if (currentValue === 0) {
     currentValue = number;
+    displayedValue.innerHTML = currentValue;
   } else {
     currentValue += number;
-  }
-  displayedNumber.innerHTML = currentValue;
+    displayedValue.innerHTML = formatCurrentValue();
+  };
 };
 
 function handleOperation(operation) {
 
 };
+
+function formatCurrentValue() {
+  let displayValue = currentValue;
+  const currentValueLength = currentValue.toString().length;
+
+  if (currentValueLength === 4) {
+    displayValue = currentValue.slice(0, 1) + ',' + currentValue.slice(1);
+  } else if (currentValueLength === 5) {
+    displayValue = currentValue.slice(0, 2) + ',' + currentValue.slice(2);
+  } else if (currentValueLength === 6) {
+    displayValue = currentValue.slice(0, 3) + ',' + currentValue.slice(3);
+  } else if (currentValueLength === 7) {
+    displayValue = currentValue.slice(0, 1) + ',' + currentValue.slice(1, 4) + ',' + currentValue.slice(4);
+  } else if (currentValueLength === 8) {
+    displayValue = currentValue.slice(0, 2) + ',' + currentValue.slice(2, 5) + ',' + currentValue.slice(5);
+  } else if (currentValueLength === 9) {
+    displayValue = currentValue.slice(0, 3) + ',' + currentValue.slice(3, 6) + ',' + currentValue.slice(6);
+  }
+  return displayValue;
+};
+
 
 
 
